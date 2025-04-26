45bc7196cd0f6b8a63fe3de4496e9e4726964c27
donggcao18 <caovdongg18@gmail.com>
2025-04-26 23:01:15 +0700

    ✅update qaAdvisor

--- java-src/src/main/java/org/example/QuestionAdviser.java
+++ java-src/src/main/java/org/example/QuestionAdviser.java
@@ -7,24 +7,54 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.qdrant.QdrantVectorStore;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class QuestionAdviser {
     private VertexAiGeminiChatModel chatModel;
     private VectorStore vectorStore;
+    private QuestionAnswerAdvisor qaAdvisor;
 
-    public QuestionAdviser() {
+    public QuestionAdviser(String storeName) throws IOException, ExecutionException, InterruptedException {
+        String projectId = System.getenv("VERTEX_AI_GEMINI_PROJECT_ID");
+        String location = System.getenv("VERTEX_AI_GEMINI_LOCATION");
+        VertexAiGeminiChatModel chatModel = new ChatModel(projectId, location).getChatModel();
         this.chatModel = chatModel;
+
+        QdrantVectorStore vectorStore = new VectorDatabase(storeName).getVectorStore();
+        this.vectorStore = vectorStore;
+
+        QuestionAnswerAdvisor qaAdvisor = new QuestionAnswerAdvisor(this.vectorStore,
+                SearchRequest.builder().
+                        similarityThreshold(0.8d).
+                        topK(6).
+                        build());
+        this.qaAdvisor = qaAdvisor;
     }
-    QuestionAnswerAdvisor qaAdvisor = new QuestionAnswerAdvisor(this.vectorStore,
-            SearchRequest.builder().
-                    similarityThreshold(0.8d).
-                    topK(6).
-                    build());
-    String userText = "What is the best laptop for gaming?";
-    ChatResponse response = ChatClient.builder(chatModel)
-            .build().prompt()
-            .advisors(new QuestionAnswerAdvisor(vectorStore))
-            .user(userText)
-            .call()
-            .chatResponse();
+
+
+    public ChatResponse advise(String userText) {
+        ChatResponse response = ChatClient.builder(this.chatModel)
+                .build().prompt()
+                .advisors(new QuestionAnswerAdvisor(this.vectorStore))
+                .user(userText)
+                .call()
+                .chatResponse();
+
+        return response;
+    }
+
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        String storeName = "laptop";
+        QuestionAdviser adviser = new QuestionAdviser(storeName);
+        String userText = "hello ";
+        ChatResponse response = adviser.advise(userText);
+
+        System.out.println(response);
+    }
+
+
+
 }
