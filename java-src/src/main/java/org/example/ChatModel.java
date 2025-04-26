5204a8a29e2017b6515c786db5e7c496e87f4750
donggcao18 <caovdongg18@gmail.com>
2025-04-26 22:59:02 +0700

    ✅update chat model

--- java-src/src/main/java/org/example/ChatModel.java
+++ java-src/src/main/java/org/example/ChatModel.java
@@ -11,13 +11,18 @@ import java.util.*;
 // need to set GOOGLE_APPLICATION_CREDENTIALS = "path/to/your/credentials.json"
 
 public class ChatModel {
-    private final VertexAI vertexApi;
-    private final VertexAiGeminiChatModel chatModel;
-
-    public ChatModel(String projectId, String location) {
-        this.vertexApi = new VertexAI(projectId, location);
-
-        /*
+    private VertexAI vertexApi;
+    private VertexAiGeminiChatModel chatModel;
+    private final VertexAiGeminiChatOptions OPTION = VertexAiGeminiChatOptions.builder()
+            .temperature(0.7)
+            .topP(0.8)
+            .topK(40)
+            .maxOutputTokens(2048)
+            .stopSequences(List.of())
+            .candidateCount(1)
+            .model(VertexAiGeminiChatModel.ChatModel.GEMINI_2_0_FLASH)
+            .build();
+            /* 1st tuning option
         VertexAiGeminiChatOptions options = VertexAiGeminiChatOptions.builder()
                 .temperature(0.2)
                 .topP(0.2)
@@ -26,19 +31,23 @@ public class ChatModel {
                 .model(VertexAiGeminiChatModel.ChatModel.GEMINI_2_0_FLASH)
                 .build();
         */
+        /* 2nd tuning option
         VertexAiGeminiChatOptions options = VertexAiGeminiChatOptions.builder()
-                .temperature(0.7)
-                .topP(0.8)
+                .temperature((double)0.7F)
+                .topP((double)1.0F)
                 .topK(40)
-                .maxOutputTokens(2048)
+                .maxOutputTokens(500)
                 .stopSequences(List.of())
                 .candidateCount(1)
                 .model(VertexAiGeminiChatModel.ChatModel.GEMINI_2_0_FLASH)
                 .build();
+        */
 
+    public ChatModel(String projectId, String location) {
+        this.vertexApi = new VertexAI(projectId, location);
         this.chatModel = VertexAiGeminiChatModel.builder()
                 .vertexAI(vertexApi)
-                .defaultOptions(options)
+                .defaultOptions(OPTION)
                 .build();
     }
 
@@ -46,20 +55,21 @@ public class ChatModel {
         return this.chatModel;
     }
 
-    public ChatResponse call(String userPrompt) {
+    public ChatResponse generate(String userPrompt) {
         Prompt chatPrompt = new Prompt(userPrompt);
         return this.chatModel.call(chatPrompt);
     }
 
+    //test the chat model
     public static void main(String[] args) {
         String projectId = System.getenv("VERTEX_AI_GEMINI_PROJECT_ID");
         String location = System.getenv("VERTEX_AI_GEMINI_LOCATION");
 
-        System.out.println("GOOGLE_CLOUD_PROJECT_ID: " + projectId);
-        System.out.println("GOOGLE_CLOUD_LOCATION: " + location);
+        System.out.println("VERTEX_AI_GEMINI_PROJECT_ID: " + projectId);
+        System.out.println("VERTEX_AI_GEMINI_LOCATION: " + location);
 
         ChatModel chatModel = new ChatModel(projectId, location);
-        ChatResponse response = chatModel.call("Generate the names of 5 famous national presidents.");
+        ChatResponse response = chatModel.generate("Can you explain Polynorpishm in OOP in detail");
         System.out.println(response);
     }
 
